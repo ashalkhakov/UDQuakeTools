c87e6c9c160b279d6c4d2d2254196e988cb913f5
Artyom Shalkhakov <artyom.shalkhakov@gmail.com>
2026-06-30 10:37:03 +0500

    Cache linuxdeploy.

--- Scripts/appimage/setup-linuxdeploy.sh
+++ Scripts/appimage/setup-linuxdeploy.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
+# If the patched linuxdeploy is already there (from cache/artifact), do nothing
+if [ -d "squashfs-root" ]; then
+    echo "Linuxdeploy already provisioned. Skipping download."
+    exit 0
+fi
+
 wget -q https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
 wget -q https://github.com/linuxdeploy/linuxdeploy-plugin-appimage/releases/download/continuous/linuxdeploy-plugin-appimage-x86_64.AppImage
 chmod +x linuxdeploy*.AppImage

0030933246f73e55ab5991ed4d56d119c051812c
Artyom Shalkhakov <artyom.shalkhakov@gmail.com>
2026-07-09 18:20:36 +0500

    Try another way.

--- Scripts/package-appimage.sh
+++ Scripts/package-appimage.sh
@@ -18,8 +18,6 @@ done
 export OUTPUT="UDQuakeTools-Linux-${APP_VERSION:-dev}.AppImage"
 export APPIMAGE_EXTRACT_AND_RUN=1
 export NO_VALIDATE=1
-export APPIMAGETOOL_SIGN_ARGS="--runtime-file /tmp/appimage-runtime/runtime-x86_64"
 
 LD_LIBRARY_PATH="${LOCAL_PREFIX}/System/Library/Libraries:${LOCAL_PREFIX}/Local/Library/Libraries:${WORKSPACE_DIR}/AppDir/usr/lib:${LD_LIBRARY_PATH:-}" \
-    /usr/local/lib/linuxdeploy/AppRun --appdir AppDir "${ELF_ARGS[@]}" --output appimage
-
+    /usr/local/lib/linuxdeploy/AppRun --appdir AppDir "${ELF_ARGS[@]}" --runtime-file /tmp/appimage-runtime/runtime-x86_64 --output appimage

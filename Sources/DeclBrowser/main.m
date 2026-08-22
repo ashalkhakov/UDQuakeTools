7d1e5a8abb758d78e1326f235e01900e8ae52329
Artyom Shalkhakov <artyom.shalkhakov@gmail.com>
2026-08-22 23:49:41 +0500

    Add a crash report on GNUstep.

--- Sources/DeclBrowser/main.m
+++ Sources/DeclBrowser/main.m
@@ -5,6 +5,16 @@
 
 #import <AppKit/AppKit.h>
 
+#if !defined(__APPLE__)
+#import "UDCrashReporter.h"
+#endif
+
 int main(int argc, const char *argv[]) {
+#if !defined(__APPLE__)
+    // Linux: print a backtrace on segfault/uncaught exception instead of
+    // dying silently. No-op on macOS (guarded so the Xcode target does not
+    // need the file at all).
+    UDInstallCrashDiagnostics();
+#endif
     return NSApplicationMain(argc, argv);
 }
